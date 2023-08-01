c97cc89440330130d61fb6b2c8aafa0ab907f0d4
agrawalprakash <33377123+agrawalprakash@users.noreply.github.com>
2023-08-01 12:33:41 +0530

    Minimum number of vertices to traverse Graph

---
+++ Simple/src/app/MinimumNumberOfVertices.java
@@ -0,0 +1,36 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinimumNumberOfVertices {
+    
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        List<Integer>[] g = new ArrayList[n];
+
+        for (int i = 0; i < n; i++) {
+
+            g[i] = new ArrayList();
+ 
+        }
+
+        int[] inDegree = new int[n];
+
+        for (List<Integer> e : edges) {
+            g[e.get(0)].add(e.get(1));
+            inDegree[e.get(1)]++;
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i= 0; i < n; i++) {
+
+            if (inDegree[i] == 0)
+               result.add(i);
+
+        }
+
+        return result;
+    }
+
+}

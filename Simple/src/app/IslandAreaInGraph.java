6e2c65857d511a164caedbc6a8b9f4457dc7da62
agrawalprakash <33377123+agrawalprakash@users.noreply.github.com>
2023-09-18 11:46:26 +0530

    Max Area of Island Graph

---
+++ Simple/src/app/IslandAreaInGraph.java
@@ -0,0 +1,37 @@
+package app;
+
+public class IslandAreaInGraph {
+
+    public int maxAreaOfIsland(int[][] grid) {
+
+        int area = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+
+                if (grid[i][j] == 1) {
+                    area = Math.max(area, dfs(grid, i, j));
+                }
+
+            }
+        }
+
+        return area;
+        
+    }
+
+    private int dfs(int[][] grid, int i, int j) {
+
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != 1) {
+
+            return 0;
+
+        }
+
+        grid[i][j] = -1;
+
+        return 1 + dfs(grid, i-1, j) + dfs(grid, i+1, j) + dfs(grid, i, j-1) + dfs(grid, i, j+1);
+
+    }
+    
+}

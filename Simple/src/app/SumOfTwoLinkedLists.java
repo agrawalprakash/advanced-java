7dee7641c3e699b2cf6306358c4213605e133f82
agrawalprakash <33377123+agrawalprakash@users.noreply.github.com>
2023-10-19 09:21:32 +0530

    Sum of two linked list efficient solution

--- Simple/src/app/SumOfTwoLinkedLists.java
+++ Simple/src/app/SumOfTwoLinkedLists.java
@@ -1,84 +1,47 @@
-import java.util.Stack;
-
-public class SumOfTwoLinkedLists {
-    
-
- public class ListNode {
+package app;
+class ListNode {
       int val;
       ListNode next;
       ListNode() {}
       ListNode(int val) { this.val = val; }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
+ }
  
-
+class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         
-        ListNode rl1 = reverseList(l1);
-        ListNode rl2 = reverseList(l2);
-
-        Stack<Integer> stack1 = convertListToStack(rl1);
-        Stack<Integer> stack2 = convertListToStack(rl2);
-                
-
-        ListNode head = null;
-
+        ListNode dummy = new ListNode(0);
+        
+        ListNode temp = dummy;
         
         int carry = 0;
-
-        while (!stack1.isEmpty() || !stack2.isEmpty() || carry != 0) {
-
+        
+        while (l1 != null || l2 != null || carry > 0) {
+            
             int sum = carry;
-
-            if (!stack1.isEmpty()) { sum += stack1.pop(); }
-            if (!stack2.isEmpty()) { sum += stack2.pop(); }
-
-            ListNode node1 = new ListNode(sum%10);
-            carry = sum / 10;
-
-            node1.next = head;
-            head = node1;
-
-        }
-
-        return reverseList(head);
-
-    }
-
-    private Stack<Integer> convertListToStack(ListNode list1) {
-
-            Stack<Integer> stack = new Stack<>();
-
-            while (list1 != null) {
-
-                stack.add(list1.val);
-                list1 = list1.next;
-
+            
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
             }
-
-            return stack;
-
-    }
-    
-    private ListNode reverseList(ListNode head) {
-        
-                
-        ListNode curr = head;
-        ListNode prev = null;
-        
-        while (curr != null) {
             
-            ListNode temp = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = temp;
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            
+            carry = sum / 10;
+            
+            ListNode newNode = new ListNode(sum%10);
+            
+            temp.next = newNode;
+            
+            temp = temp.next;
             
         }
         
-        return prev;
-        
+        return dummy.next;
+         
         
     }
-
-
 }

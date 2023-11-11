6ad87df9d97cf05f0880e867e68fbdb45bc1c83f
agrawalprakash <33377123+agrawalprakash@users.noreply.github.com>
2023-11-11 08:43:00 +0530

    Index Of First Occurrence Of Sting

---
+++ Simple/src/app/IndexOfFirstOccurrenceOfSting.java
@@ -0,0 +1,25 @@
+package app;
+
+public class IndexOfFirstOccurrenceOfSting {
+    public int strStr(String haystack, String needle) {
+        
+        
+        for (int i = 0; i < (haystack.length() - needle.length() + 1); i++) {
+            
+            
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                
+                if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                    return i;
+                }
+                
+            }
+            
+            
+        }
+        
+        return -1;
+        
+        
+    }
+}

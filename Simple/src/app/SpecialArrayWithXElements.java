8d5315995a163249595b4261c046c0ddfbb494af
agrawalprakash <33377123+agrawalprakash@users.noreply.github.com>
2023-12-03 08:45:08 +0530

    Special Array with X Elements

---
+++ Simple/src/app/SpecialArrayWithXElements.java
@@ -0,0 +1,34 @@
+package app;
+
+public class SpecialArrayWithXElements {
+    
+    public int specialArray(int[] nums) {
+        
+        int[] bucket = new int[1001];
+        
+        for (int num : nums) {
+            
+            bucket[num]++;
+            
+            
+        }
+        
+        int total = nums.length;
+        
+        for (int i = 0; i < 1001; i++) {
+            
+            
+            if (i == total) {
+                return i;
+            }
+            
+            
+            total = total - bucket[i];
+        }
+        
+        return -1;
+        
+        
+    }
+
+}

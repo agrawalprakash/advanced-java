a701b10da85d9a853e553fd44d552a8e3e64bf84
agrawalprakash <33377123+agrawalprakash@users.noreply.github.com>
2023-12-05 07:45:41 +0530

    Largest Number from Array Java

---
+++ Simple/src/app/LargestNumberFromArray.java
@@ -0,0 +1,61 @@
+package app;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class LargestNumberFromArray {
+        public String largestNumber(int[] nums) {
+        
+        
+        if (nums.length == 0) {
+            
+            return "";
+            
+        }
+        
+        String[] sa = new String[nums.length];
+        
+        for (int i = 0; i < nums.length; i++) {
+            
+            sa[i] = Integer.toString(nums[i]);
+            
+        }
+        
+        Arrays.sort(sa, new Comparator<String>() {
+            
+                            
+                            public int compare(String a, String b) {
+                                
+                               String o1 = a + b;
+                               String o2 = b + a;
+                                
+                               return o2.compareTo(o1);
+                                
+                            }
+                        
+                        
+                        }
+                   
+                   
+                   
+                   );
+        
+        if (sa[0].equals("0")) {
+           return "0";
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        
+        for (String s : sa) {
+            
+            sb.append(s);
+            
+        }
+        
+        return sb.toString();
+        
+        
+        
+    }
+}

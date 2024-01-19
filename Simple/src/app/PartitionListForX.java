414e1d35197581fdad8b8b6a14fc7601f34f5662
agrawalprakash <33377123+agrawalprakash@users.noreply.github.com>
2024-01-19 08:13:02 +0530

    Partition List Fox X

---
+++ Simple/src/app/PartitionListForX.java
@@ -0,0 +1,47 @@
+package app;
+
+public class PartitionListForX {
+    public class ListNode {
+             int val;
+             ListNode next;
+             ListNode() {}
+             ListNode(int val) { this.val = val; }
+             ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+        }
+        
+       class Solution {
+           public ListNode partition(ListNode head, int x) {
+               
+               ListNode small = new ListNode(0);
+               ListNode high = new ListNode(0);
+               
+               ListNode smallHead = small;
+               ListNode highHead = high;
+               
+               while (head != null) {
+                   
+                   
+                   if (head.val < x) {
+                       
+                       smallHead.next = head;
+                       
+                       smallHead = smallHead.next;
+                       
+                   } else {
+                       
+                       highHead.next = head;
+                       
+                       highHead = highHead.next;
+                   }
+                   
+                   head = head.next;
+                   
+               }
+               
+               highHead.next = null;
+               
+               smallHead.next = high.next;
+               
+               return small.next;
+           }
+}

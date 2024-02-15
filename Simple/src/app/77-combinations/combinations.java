f1ee6bcffa93b2de85edcdf2bb1e008e6cded4cc
agrawalprakash <33377123+agrawalprakash@users.noreply.github.com>
2024-02-15 09:43:50 +0530

    Time: 18 ms (75.58%) | Memory: 93.9 MB (83.28%) - LeetSync

---
+++ Simple/src/app/77-combinations/combinations.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        
+        List<List<Integer>> result = new ArrayList<>();
+        
+        generateSubSets(1, n, new ArrayList(), result, k);
+        
+        return result;
+        
+    }
+    
+    private void generateSubSets(int start, int n, List<Integer> current, List<List<Integer>> result, int k) {
+        
+        
+        if (current.size() == k) {
+            
+            result.add(new ArrayList(current));
+         
+            return;
+        }
+        
+        for (int i = start; i <= n; i++) {
+            
+            current.add(i);
+            
+            generateSubSets(i+1, n, current, result, k);
+            
+            current.remove(current.size()-1);
+        }
+        
+    }
+}

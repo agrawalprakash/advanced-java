130b53a876739c87f6d76461fda9cdc20d10f741
agrawalprakash <33377123+agrawalprakash@users.noreply.github.com>
2024-06-21 07:17:54 +0530

    Check if Valid BST 2

---
+++ Simple/src/app/CheckIfValidBST2.java
@@ -0,0 +1,35 @@
+public class CheckIfValidBST2 {
+    
+
+    class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode (int val) {
+
+            this.val = val;
+        }
+
+    }
+
+    public boolean isValidBST (TreeNode root) {
+
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+
+    }
+
+    private boolean isValidBST (TreeNode root, long minVal, long maxVal) {
+
+        if (root == null) return true;
+
+        if (root.val <= minVal || root.val >= maxVal) {
+            return false;
+        }
+
+        return isValidBST(root.left, minVal, root.val) && 
+               isValidBST(root.right, root.val, maxVal);
+
+    }
+}

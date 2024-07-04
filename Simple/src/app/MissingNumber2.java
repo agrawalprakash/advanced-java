24c8da9762cded5b01dd7de1338040723343903e
agrawalprakash <33377123+agrawalprakash@users.noreply.github.com>
2024-07-04 11:08:14 +0530

    Java missing number solution

---
+++ Simple/src/app/MissingNumber2.java
@@ -0,0 +1,26 @@
+package app;
+
+public class MissingNumber2 {
+
+    public int missingNumber(int[] nums) {
+        
+        int len = nums.length;
+        
+        int sum = 0;
+        
+        for (int num : nums) {
+            
+            sum += num;
+        }
+        
+        int total = (len*(len+1))/2;
+        
+        if (total == sum) return 0;
+        
+        
+        return total - sum;
+        
+        
+    }
+    
+}
